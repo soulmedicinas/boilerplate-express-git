76066dfaa624e2a7b3840bc849ef1cc8c401483c
Georginio <132018437+Georginio-prod@users.noreply.github.com>
2025-05-27 22:20:46 +0000

    test:add test

--- myApp.js
+++ myApp.js
@@ -1,39 +1,22 @@
-let express = require('express');
+let express = require("express");
+require("dotenv").config();
 let app = express();
 
+console.log("HRBP  Hello World");
 
+app.get("/json", (req, res) => {
+  //res.json({ message: "Hello json" });
 
+  console.log("log de .env", process.env.MESSAGE_STYLE);
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  }
 
+  let absolutePath = __dirname + "/views/index.html";
 
+  res.sendFile(absolutePath);
+});
 
+// app.use("/public", express.static(__dirname + "/public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
