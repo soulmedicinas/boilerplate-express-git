2e6e0adb9b73a8d8b13f159b8fa52b5e50529d43
tira_menali <77180304+soulmedicinas@users.noreply.github.com>
2025-07-02 17:17:41 -0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,26 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
+console.log("Hello World");
 
+//app.get('/', function(req, res) {
+    //res.send('Hello Express');
+//});
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/json', function(req, res) {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ "message" : "HELLO JSON"})}
+  else {
+    res.json({ "message": "Hello json"})
+  };
+});
 
 
 
